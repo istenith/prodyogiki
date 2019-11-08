238dddde93cf5d9787c32dbb17d7d420f176c480
WinstonWolf1 <rohannawathe@hotmail.com>
2019-11-09 00:36:54 +0530

    Added variable build directory to build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,38 @@
-#! bash
+#!/bin/bash
 trap "kill 0" EXIT
 
-BUILDS_DIR = builds
+# Function to change directory in less-watch-compiler.config.json
+change_less_watch_json() {
+parse_json_script=$(mktemp parse_json.XXXX.py)
+cat > $parse_json_script << SCRIPT
+#!/usr/bin/env python
+import json
+filename = "less-watch-compiler.config.json"
+with open(filename, 'r+') as f:
+  jsonObject = json.load(f)
+  jsonObject['outputFolder'] = "${BUILD_DIR}/styles"
+  f.seek(0)
+  json.dump(jsonObject, f, indent=4, separators=(','," : "))
+SCRIPT
+python $parse_json_script && rm $parse_json_script
+}
 
-mkdir -p builds
-mkdir -p builds/styles
-mkdir -p builds/scripts
-mkdir -p builds/resources
+# Set this variable to the name of the output directory
+BUILD_DIR=builds
 
-cp src/scripts/* builds/scripts/ --update
-cp -r src/resources/* builds/resources/ --update
-cp src/root/* builds/ --update
+mkdir -p ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}/styles
+mkdir -p ${BUILD_DIR}/scripts
+mkdir -p ${BUILD_DIR}/resources
 
-pug -w src/pug/pages/ --basedir ./ --out builds/ &
+cp src/scripts/* ${BUILD_DIR}/scripts/ --update
+cp -r src/resources/* ${BUILD_DIR}/resources/ --update
+cp src/root/* ${BUILD_DIR}/ --update
+
+change_less_watch_json
+
+pug -w src/pug/pages/ --basedir ./ --out ${BUILD_DIR}/ &
 less-watch-compiler &
-live-server --port=9600 ./builds &
+live-server --port=9600 ./${BUILD_DIR} &
 
 wait
