d255e36cf7bf327936ddda250215c7bdb30b9210
Parth Pant <parthpant4@gmail.com>
2019-12-07 14:14:08 +0530

    [UPDATE] make flicker similar to tubelight

---
+++ src/scripts/flicker.js
@@ -0,0 +1,19 @@
+//find the text i want to flicker
+const text = document.getElementById("flicker");
+console.log(text);
+
+const getDuration = () => {
+    //find a random time interval b/w 0 to 0.5 sec
+    const randVal = Math.random()*800+1;
+    console.log(randVal);
+    return randVal;
+};
+
+const doFlicker = () => {
+    //logic to invert the opacity
+    text.classList.toggle('clear');
+        
+    setTimeout(doFlicker, getDuration());  
+};
+
+setTimeout(doFlicker, getDuration());
