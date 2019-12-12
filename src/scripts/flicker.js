93c12c5b54c5d612651ce38a1df5185132351647
Parth Pant <parthpant4@gmail.com>
2019-12-12 15:56:50 +0530

    fix #36

--- src/scripts/flicker.js
+++ src/scripts/flicker.js
@@ -2,19 +2,11 @@
 const text = document.getElementById("flicker");
 console.log(text);
 
-const getDuration = () => {
-    //find a random time interval b/w 0 to 0.5 sec
-    const randVal = Math.random()*500+1;
-  
-    // console.log(randVal);
-    return randVal;
-};
-
 const doFlicker = () => {
     //logic to invert the opacity
     text.classList.toggle('clear');
         
-    setTimeout(doFlicker, getDuration());  
+    setTimeout(doFlicker, (Math.random()*500)+1);  
 };
 
-setTimeout(doFlicker, getDuration());
+setTimeout(doFlicker, (Math.random()*500)+1);
