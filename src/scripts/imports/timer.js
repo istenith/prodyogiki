399e05cef716c52c774f9e9d5ce8930e071e5242
Sumit Dhiman <sd08012003@gmail.com>
2021-01-26 14:48:33 +0530

    changed time

--- src/scripts/imports/timer.js
+++ src/scripts/imports/timer.js
@@ -1,6 +1,6 @@
 export function countdown() {
 	var now = new Date();
-	var eventdate = new Date('January 24, 2020 21:00:00');
+	var eventdate = new Date('February 12, 2021 5:00:00');
 
 	var currenttime = now.getTime();
 	var eventtime = eventdate.getTime();
