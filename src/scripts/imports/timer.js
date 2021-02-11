ee98a6ca0a96365c4a1dbd85743b3f02efeffa83
Gaurav Pandey <gauravguitarrocks@gmail.com>
2021-02-11 17:12:21 +0530

    Change time for event.

--- src/scripts/imports/timer.js
+++ src/scripts/imports/timer.js
@@ -1,6 +1,6 @@
 export function countdown() {
     var now = new Date();
-    var eventdate = new Date('February 12, 2021 00:00:00');
+    var eventdate = new Date('February 12, 2021 17:00:00');
 
     var currenttime = now.getTime();
     var eventtime = eventdate.getTime();
